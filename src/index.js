3f23e7227e5d7b4f5a18f683c812c95f8722f369
github username <github email address>
2018-08-28 15:58:14 -0400

    Added redux developer tools to index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import { createStore } from 'redux';
 import reducer from "./reducers/reducer"
 import { Provider } from "react-redux"
 import { BrowserRouter } from 'react-router-dom'
+import { devToolsEnhancer } from 'redux-devtools-extension';
 // import 'semantic-ui-css/semantic.min.css'
 
-const store = createStore(reducer)
+const store = createStore(reducer, /* preloadedState, */ devToolsEnhancer(
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+));
 
 ReactDOM.render(<BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
